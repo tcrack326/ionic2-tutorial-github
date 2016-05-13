0d6cfa7791630d2480539c8383b1676403230ab0
Thomas McCracken <thomas.mccracken@soltech.net>
2016-05-13 07:33:26 -0400

    first commit

---
+++ app/pages/home/home.ts
@@ -0,0 +1,35 @@
+import {Page, NavController} from 'ionic-angular';
+
+import {DetailsPage} from '../details/details';
+import {GitHubService} from '../../services/github';
+
+@Page({
+  templateUrl: 'build/pages/home/home.html',
+  providers: [GitHubService]
+})
+
+export class HomePage {
+  public foundRepos;
+  public username;
+
+  constructor(
+      private github: GitHubService,
+      private nav: NavController) {
+
+  }
+
+  getRepos() {
+    this.github.getRepos(this.username).subscribe(
+        data => {
+          this.foundRepos = data.json();
+        },
+        err => console.error(err),
+        () => console.log('getRepos completed')
+    );
+  }
+
+  goToDetails(repo) {
+    this.nav.push(DetailsPage, { repo: repo });
+  }
+  
+}
